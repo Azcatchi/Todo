6b27ecbeb24f96df8982fa7f542d0eadbf488cc7
Tcharlyson <tcharlyson.platel@ynov.com>
2016-12-04 17:46:42 +0100

    Debug final FDP

--- Tod/routes/index.js
+++ Tod/routes/index.js
@@ -2,29 +2,28 @@ const express = require('express');
 var router = express.Router();
 const UserFct = require('../models/users.js');
 
+// router.use(function(req, res, next)
+// {
+//   if(req.cookies.accessToken)
+//   {
+//     UserFct.checkCookieIntoDatabase(req.cookies).then((isValid) => {
+//       if(isValid)
+//       {
+//         next();
+//       } else {
+//         res.clearCookie("accessToken");
+//         res.redirect("/?session=false");
+//       }
+//
+//     });
+//   } else {
+//     res.clearCookie("accessToken");
+//     res.redirect('/users');
+//   }
+// });
 
-
-router.use(function(req, res, next)
-{
-  console.log(req.cookies);
-  if(req.cookies.accessToken)
-  {
-    UserFct.checkCookieIntoDatabase(req.cookies).then((isValid) => {
-      if(isValid)
-      {
-        res.redirect("/todos");
-      } else {
-        res.clearCookie("accessToken");
-        res.redirect("/?session=false");
-      }
-
-    });
-  }
-    next();
-});
-
-/* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log('smabite');
   var success = "";
 
   if(req.query.valid == "true")
@@ -38,46 +37,6 @@ router.get('/', function(req, res, next) {
     success = "Votre session est expirée ! Veuillez-vous reconnecter";
   }
 
-  res.render('index', { success });
-});
-
-router.get('/create', function(req, res, next) {
-  var error = "";
-
-  if(req.query.valid == "false")
-  {
-    error = "Une erreur est survenue veuillez recommencer.";
-  }
-  res.render('users/create', { error });
-});
-
-
-router.post('/create', function(req, res, next) {
-  if (!req.body.username || req.body.username === '' ||!req.body.password || req.body.password === '' ||!req.body.role || req.body.role === '') {
-    res.redirect('/create?valid=false');
-  }
-
-  UserFct.insertIntoDatabase(req.body).then(() => {
-    res.redirect('/?valid=true');
-  }).catch((err) => {
-    res.redirect('/create?valid=false');
-  });
-});
-
-router.post('/login', function(req, res, next) {
-  if (!req.body.username || req.body.username === '' ||!req.body.password || req.body.password === '') {
-    res.redirect('/?valid=false');
-  }
-  UserFct.findOneUserLogin(req.body).then((cookieArray) => {
-    res.format({
-      html : () => {
-        res.cookie('accessToken',cookieArray.accessToken).redirect('/todos');
-      },
-      json : () => {
-        res.send({'accessToken' : cookieArray.accessToken});
-      }
-    });
-  });
 });
 
 module.exports = router;
--- Tod/routes/todos.js
+++ Tod/routes/todos.js
@@ -2,37 +2,34 @@ const express = require('express');
 var router = express.Router();
 const UserFct = require('../models/users.js');
 
-router.use(function(req, res, next)
-{
-  console.log(req.cookies);
-  if(req.cookies.accessToken)
-  {
-    UserFct.checkCookieIntoDatabase(req.cookies).then((isValid) => {
-      if(isValid)
-      {
-        res.redirect("/todos");
-      } else {
-        res.clearCookie("accessToken");
-        res.redirect("/?session=false");
-      }
+// router.use(function(req, res, next)
+// {
+//   if(req.cookies.accessToken)
+//   {
+//     UserFct.checkCookieIntoDatabase(req.cookies).then((isValid) => {
+//       if(isValid)
+//       {
+//         next();
+//       } else {
+//         res.clearCookie("accessToken");
+//         res.redirect("/?session=false");
+//       }
+//
+//     });
+//   }
+// });
 
-    });
-  }
-    next();
-});
-
- router.get('/', function(req, res, next) {
-   // On affiche les todos assignées à l'utilisateur 
+router.get('/', function(req, res, next) {
+   // On affiche les todos assignées à l'utilisateur
    // Un boutton pour les modifier -> /edit/:id  Voir pour
    // Checkbox de finition de todo
-
    res.send("caca");
- });
+});
 
  router.post('/add', function(req, res, next) {
-   res.send("caca");
+   res.render('pages/addTodo', { success: "lol" });
    // Enregistre une todo et on redirige vers todos
- });
+});
 
 
 module.exports = router;
